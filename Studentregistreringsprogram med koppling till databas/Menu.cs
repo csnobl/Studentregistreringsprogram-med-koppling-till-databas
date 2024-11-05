2d03ddad0bfb18a2922a1d62f0c4f523f8e2eb7d
Christoffer Snobl <csnobl@hotmail.com>
2024-11-05 16:25:41 +0100

    Intriduced some empty lines in the menu.

--- Studentregistreringsprogram med koppling till databas/Menu.cs
+++ Studentregistreringsprogram med koppling till databas/Menu.cs
@@ -32,41 +32,53 @@ namespace Studentregistreringsprogram_med_koppling_till_databas
                 switch (Convert.ToInt32(Console.ReadLine()))
                 {
                     case 1:
-                        Console.WriteLine("Please enter the first name!\n");
+                        Console.WriteLine();
+
+                        Console.WriteLine("Please enter the first name!");
                         string firstName = Console.ReadLine();
 
-                        Console.WriteLine("Please enter the last name!\n");
+                        Console.WriteLine("Please enter the last name!");
                         string lastName = Console.ReadLine();
 
                         Console.WriteLine("Please enter the city!");
                         string city = Console.ReadLine();
-                        
+
+                        Console.WriteLine();
+
                         ControllerObject.CreateNewStudent(firstName, lastName, city);
                         break;
 
                     case 2:
-                        Console.WriteLine("Please enter the id of the student!\n");
+                        Console.WriteLine();
+
+                        Console.WriteLine("Please enter the id of the student!");
                         int idChoice = Convert.ToInt32(Console.ReadLine());
 
                         if (!ControllerObject.StudentExist(idChoice))
                         {
-                            Console.WriteLine("Please enter a valid number, you can always look in the list");
+                            Console.WriteLine();
+                            Console.WriteLine("Please enter a valid number, you can always look in the list!");
+                            Console.WriteLine();
                             break;
                         }
 
-                        Console.WriteLine("Please update first name!\n");
+                        Console.WriteLine("Please update first name!");
                         string updatedFirstName = Console.ReadLine();
 
-                        Console.WriteLine("Please update last name!\n");
+                        Console.WriteLine("Please update last name!");
                         string updatedLastName = Console.ReadLine();
 
                         Console.WriteLine("Please update city!");
                         string updatedCity = Console.ReadLine();
+
+                        Console.WriteLine();
                         
                         ControllerObject.UpdateStudent(idChoice, updatedFirstName, updatedLastName, updatedCity);
                         break;
 
                     case 3:
+                        Console.WriteLine();
+
                         List<Student> studentList = ControllerObject.GetStudents();
                         
                         foreach (Student student in studentList)
@@ -84,7 +96,12 @@ namespace Studentregistreringsprogram_med_koppling_till_databas
                         break;
 
                     default:
+                        Console.WriteLine();
+
                         Console.WriteLine("Please choose a number between 1 and 4!");
+
+                        Console.WriteLine();
+
                         break;
                 }
             }
